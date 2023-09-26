a7a035daf6f610a82aaee6758a1d501bafa9a29b
PJ <92328553+TheRoyalOnce@users.noreply.github.com>
2023-09-26 10:56:22 -0600

    Main page changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { Button } from "@nextui-org/react";
 
-function App () {
+function App() {
   return (
     <>
-      <h1>App</h1>
+      <div className="md:h-full h-screen md:px-12 px-4 flex">
+        <div className="m-auto flex flex-col py-3 gap-y-5">
+          <div className="text-purple-400 font-bold flex">
+            <span className="mx-auto text-5xl">Welcome!</span>
+          </div>
+          <div className="max">
+            <img src="src\assets\eSellerate.png" alt="eSellerate logo" />
+          </div>
+          <div className="px-10 flex justify-between">
+            <div>
+              <Button
+                radius="full"
+                className="bg-gradient-to-tr from-purple-500 to-pink-500 text-white shadow-lg"
+              >
+                Login
+              </Button>
+            </div>
+            <div>
+              <Button
+                radius="full"
+                className="bg-gradient-to-tr from-violet-500 to-fuchsia-500 text-white shadow-lg"
+              >
+                Register
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
