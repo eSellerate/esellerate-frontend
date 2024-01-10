284c9a83f2fc46beac30c032a88bc6da85dab9ae
Feivian4 <feivian21@gmail.com>
2024-01-09 22:52:44 -0600

    chatpanel

--- src/components/Chat/ChatPanel.jsx
+++ src/components/Chat/ChatPanel.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 
 import { Chip } from "@nextui-org/chip"
 
-export default function ChatPanel() {
+export default function ChatPanel(props) {
+    var messages = props.messages
 
     const MessageBlubble = (props) => {
 
@@ -13,25 +14,37 @@ export default function ChatPanel() {
             msj: PropTypes.string
         }
 
-        return(
+        return (
             <div className='w-full relative'>
                 <Chip
                     className={`client-mesage font-bold absolute ${props.position}`}
                     color={props.color}
                 >
                     {props.msj}
-                </Chip> 
+                </Chip>
             </div>
         )
     }
 
-    return(
+    function LoadMessages() {
+        if (messages.length > 0) {
+
+            return (
+                <>
+                    {messages.map((message, index) => (
+                        message.from.user_id === message.message_resources.find(function(o){ return o.name==="sellers" }).id ? (
+                            <MessageBlubble key={index} color='default' position='left-0' msj={message.text} />
+                          ) : <MessageBlubble key={index} color='primary' position='right-0' msj={message.text} />)
+                    )}
+                </>
+            )
+        }
+    }
+
+    return (
         <section className='h-full container'>
             <div className='flex flex-col gap-8'>
-                {/* Ejemplo de uso */}
-                <MessageBlubble color='primary' position='left-0' msj='hola mundo'/>
-                <MessageBlubble color='primary' position='left-0' msj='amigos de yt' />
-                <MessageBlubble color='default' position='right-0' msj='no me vuelva a escribir en su vida'/>
+                <LoadMessages />
             </div>
         </section>
     )
