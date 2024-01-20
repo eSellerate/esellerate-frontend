843d36684867dc5c0e71e48bf8cc598515ddf943
Christian Ochoa <a20110469@live.ceti.mx>
2024-01-20 00:42:18 -0600

    Boton eliminar producto ahora funciona

--- src/functions/closePublication.js
+++ src/functions/closePublication.js
@@ -1,8 +1,19 @@
 import axios from 'axios'
+import extractCookie from '../components/Utilities/Cookies/GetCookieByName'
 
 async function closePublication (ID) {
+  const token = extractCookie('session')
   try {
-    const response = await axios.get(`${import.meta.env.VITE_BACKEND_END_POINT}mercado-libre/delete?id=` + ID)
+    const response = await axios.delete(`${import.meta.env.VITE_BACKEND_END_POINT}mercado-libre/delete`,
+    {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      data: {
+        ID
+      }
+    })
     return response
   } catch (error) {
     console.log(error)
